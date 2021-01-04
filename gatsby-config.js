e7d528601e21ab2e1171a48a4c706007fce5f6e3
HugoLattard <hugolattard@gmail.com>
2021-01-04 23:29:38 +0100

    Added web-font-loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
@@ -39,6 +39,14 @@ module.exports = {
       options: {
         spaceId: `lep2h1zc06oo`,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
+      options: {
+        google: {
+          families: ["IBM Plex Sans", "Barlow"],
+        },
       },
     },
   ],
