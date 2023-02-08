b53e2f2a7fbdc6d17f75a042b048cf4bf09fbbaa
Orphee1 <hugolattard@gmail.com>
2023-02-08 13:43:08 +0100

    fix bug

--- root-wrapper.js
+++ root-wrapper.js
@@ -210,7 +210,8 @@ export const wrapRootElement = ({ element }) => {
     <Auth0Provider
       domain="dev-a2uey52c.eu.auth0.com"
       // domain={process.env.AUTH0_DOMAIN}
-      clientId={process.env.AUTH0_CLIENTID}
+      // clientId={process.env.AUTH0_CLIENTID}
+      clientId="UxZymrgIcWpZq4DpjRTL3EQiRJiW44Qm"
       authorizationParams={{
         redirect_uri: "https://hl-gatsby-deliveroo.netlify.app",
       }}
