f5870640453c5ceecef42194b87d187cfcd8dde9
Orphee1 <hugolattard@gmail.com>
2023-02-12 18:52:06 +0100

    fix bug

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from "react"
+// import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 
 export const formatPrice = num => {
   return new Intl.NumberFormat("de-DE", {
